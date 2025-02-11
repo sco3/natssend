84ab84e052b0f96329f056aa655153e329e22a0e
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-02-11 23:24:18 +0000

    only one version

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,42 @@
-use async_nats::connect;
-use async_nats::jetstream;
+use async_nats::{connect, jetstream};
 use flate2::read::GzDecoder;
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
 use tokio;
 
-#[tokio::main]
-async fn main() {
-    if args().len() < 3 {
-        println!("natssend <subject> <file> [<nats url>]");
+fn help(s: &str) -> String {
+    println!("{}", s);
+    println!("Help: natssend <subjet> <file[.gz]> [<nats_url>]");
+    return s.to_string();
+}
+
+async fn send() -> Result<(), Box<dyn std::error::Error>> {
+    let subject = args().nth(1).ok_or(help("Param: Subject"))?;
+    let filename = args().nth(2).ok_or(help("Param: File"))?;
+    let nats_url = args().nth(3).unwrap_or("nats://localhost:4222".to_string());
+    let mut data = Vec::new();
+    let mut file = File::open(&filename).unwrap();
+
+    if filename.ends_with(".gz") {
+        let mut decoder = GzDecoder::new(file);
+        decoder.read_to_end(&mut data).unwrap();
+    } else {
+        file.read_to_end(&mut data).unwrap();
     }
 
-    let subject = args().nth(1).expect("Subject to send data to");
-    let filename = args().nth(2).expect("File to send");
-    let nats_url = args().nth(3).unwrap_or("nats://localhost:4222".to_string());
+    let client = connect(nats_url).await?;
+    let jetstream = jetstream::new(client);
+    jetstream.publish(subject, data.into()).await?;
+    return Ok(());
+}
 
-    println!("File: {}", filename);
-    match File::open(filename) {
-        Ok(file) => {
-            let mut decoder = GzDecoder::new(file);
-            let mut data = Vec::new();
-
-            match decoder.read_to_end(&mut data) {
-                Ok(_) => {
-                    let client = connect(nats_url).await;
-                    match client {
-                        Ok(client) => {
-                            let jetstream = jetstream::new(client);
-                            let result = jetstream.publish(subject, data.into()).await;
-                            match result {
-                                Ok(ack) => match ack.await {
-                                    Ok(_) => {
-                                        println!("Sent and acknowledged.")
-                                    }
-                                    Err(e) => {
-                                        eprintln!("Error ack: {}", e);
-                                    }
-                                },
-                                Err(e) => {
-                                    eprintln!("Cannot send: {}", e)
-                                }
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Cannot connect to nats: {}", e);
-                        }
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Cannot ungzip: {}", e);
-                }
-            }
-        }
+#[tokio::main]
+async fn main() {
+    match send().await {
+        Ok(_) => {}
         Err(e) => {
-            eprintln!("Open file error: {}", e);
+            println!("Error: {}", e);
         }
     }
 }
